795c20ef047aa918dee4e77d0e7fb323a8da26c7
Himon Rana <himon.rana@chasacademy.se>
2018-01-27 08:35:32 +0100

    minor css change-add dialog and datepicker

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-/*$(function() {
-    
-    $(this).find('article').draggable();
-}); */
-
 $(function() {
+  
+
+
+  /* Drag around list */
   $("ul.ul-list1").sortable({
     connectWith: "ul"
   });
@@ -16,44 +15,63 @@ $(function() {
     connectWith: "ul",
   });
 
-  /*var message = $(`
-    <article class="column">
-        <div class="message-header">
-            <p>Hello World1</p>
-            <button class="delete" aria-label="delete"></button>
-        </div>
-        <div class="message-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </div>
-    </article>
-    `);
-*/
-  $(".btn1").on("click", function() {
-    var addCard = $(".text1").val();
+  /* Open a new Popup-Dialog */
+  $('.dialogBtn').click( function() {
+    $('#dialog').dialog();
+    $(this).addClass('is-loading');
+  });
 
-    $("#sortable1").append('<li class="cardList"><article class="column"><div class="message-header"><button class="delete" aria-label="delete"></button></div><div class="message-body">'+ addCard +'</div></article></li>');
+
+  /* Add new list */
+  $('.btn1').on('click', function() {
+    var addCard = $('.text1').val();
+    var clearText = $(this).closest('form');
+    clearText.find('input').val('');
+    
+    $('#sortable1').append('<li class="cardList"><article class="column"><div class="message-header"><p>Date: <input type="text" id="datepicker1"><button class="info infoBtn" aria-label="delete">edit</button></p><button class="delete" aria-label="delete"></button></div><div class="message-body">'+ addCard +'</div></article></li>');
+    $('#datepicker1').datepicker( $.datepicker.regional[ "sv" ] );
+    
+    $('.infoBtn').click( function() {
+      $('#infoDialog').dialog();
+    });
+  });
+  
+  
+  $('.card').on('click', '.delete', function(){
+      $(this).closest('li').remove();
+  });
+
+  /*--------------------------------*/
+
+  $('.btn2').on('click', function() {
+    var addCard = $('.text2').val();
+    var clearText = $(this).closest('form');
+    clearText.find('input').val('');
+
+    $('#sortable2').append('<li class="cardList"><article class="column"><div class="message-header"><p>Date: <input type="text" id="datepicker2"></p><button class="info infoBtn" aria-label="delete">edit</button><button class="delete" aria-label="delete"></button></div><div class="message-body">'+ addCard +'</div></article></li>');
+    $('#datepicker2').datepicker( $.datepicker.regional[ "sv" ] );
+    
+    $('.info').click( function() {
+      $('#infoDialog').dialog();
+    });
   });
 
   $('.card').on('click', '.delete', function(){
       $(this).closest('li').remove();
   });
 
+  /*--------------------------------*/
 
-  $(".btn2").on("click", function() {
-    var addCard = $(".text2").val();
+  $('.btn3').on('click', function() {
+    var addCard = $('.text3').val();
+    var clearText = $(this).closest('form');
+    clearText.find('input').val('');
 
-    $("#sortable2").append('<li class="cardList"><article class="column"><div class="message-header"><button class="delete" aria-label="delete"></button></div><div class="message-body">'+ addCard +'</div></article></li>');
-  });
-
-  $('.card').on('click', '.delete', function(){
-      $(this).closest('li').remove();
-  });
-
-
-  $(".btn3").on("click", function() {
-    var addCard = $(".text3").val();
-
-    $("#sortable3").append('<li class="cardList"><article class="column"><div class="message-header"><button class="delete" aria-label="delete"></button></div><div class="message-body">'+ addCard +'</div></article></li>');
+    $('#sortable3').append('<li class="cardList"><article class="column"><div class="message-header"><p>Date: <input type="text" id="datepicker3"></p><button class="info infoBtn" aria-label="delete">edit</button><button class="delete" aria-label="delete"></button></div><div class="message-body">'+ addCard +'</div></article></li>');
+    $('#datepicker3').datepicker( $.datepicker.regional[ "sv" ] );
+    $('.info').click( function() {
+      $('#infoDialog').dialog();
+    });
   });
 
   $('.card').on('click', '.delete', function(){
@@ -61,23 +79,3 @@ $(function() {
   });
 
 });
-/*
-var carHeader = $(".message-header").val();
-var cardMessage = $(".message-body").val();
-
-$("ul").on("click", ".btn", function() {
-  $("<article/>", {
-    class: "column",
-    html:
-      '<div class="message-header">' +
-      "<p>" +
-      $(".message-header").val() +
-      "</p>" +
-      '<button class="delete" aria-label="delete"></button>' +
-      "</div>" +
-      '<div class="message-body">' +
-      $(".message-body").val() +
-      "</div>"
-  }).appendTo(".articleColumn");
-});
-*/
